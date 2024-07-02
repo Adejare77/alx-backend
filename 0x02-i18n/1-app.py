fc7978beebf4ae2f8475472c8d243266b7b54e99
Adejare <adejare.arasheed@gmail.com>
2024-07-02 13:55:10 +0100

    Babel

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+""" Basic Flask app """
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+# create babel instance
+
+
+class Config:
+    """ create language class attribute """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+@babel.localeselector
+def get_locale():
+    """ Select the best match locale for the user """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    """ index page """
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
